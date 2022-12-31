10fb52274670a8ed933603f6f69753162621f4bb
Seonzoo <boy672820@gmail.com>
2022-12-31 15:21:03 +0900

    First

---
+++ README.md
@@ -0,0 +1,4 @@
+# PS Day
+하루에 한 번 PS
+
+사용 언어: Rust or JS
---
+++ io_1008/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut input: String = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let value: Vec<f64> = input
+        .as_mut_str()
+        .split_ascii_whitespace()
+        .map(|x: &str| x.parse().unwrap())
+        .collect();
+
+    println!("{:?}", value[0] / value[1]);
+}
