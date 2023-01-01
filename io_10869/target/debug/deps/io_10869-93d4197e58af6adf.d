8150535affa48ed54172e89767dd28b27c4b0dc3
Seonzoo <boy672820@gmail.com>
2023-01-01 12:32:22 +0900

    clean cache

--- io_10869/target/debug/deps/io_10869-93d4197e58af6adf.d
+++
@@ -1,5 +0,0 @@
-/Users/seondev/Projects/ps-day/io_10869/target/debug/deps/io_10869-93d4197e58af6adf: src/main.rs
-
-/Users/seondev/Projects/ps-day/io_10869/target/debug/deps/io_10869-93d4197e58af6adf.d: src/main.rs
-
-src/main.rs:
--- io_10869/target/debug/io_10869.d
+++
@@ -1,1 +0,0 @@
-/Users/seondev/Projects/ps-day/io_10869/target/debug/io_10869: /Users/seondev/Projects/ps-day/io_10869/src/main.rs
