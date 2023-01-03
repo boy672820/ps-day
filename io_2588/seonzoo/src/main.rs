44c76bf7dcaba032a9df404eaaa0762194f0b8e2
Seonzoo <boy672820@gmail.com>
2023-01-03 17:40:10 +0900

    clear 2588

--- io_2588/seonzoo/src/main.rs
+++ io_2588/seonzoo/src/main.rs
@@ -1,32 +1,16 @@
 use std::io::{self, BufRead};
 
 fn main() {
-    // let mut input = String::new();
-
-    // std::io::stdin().read_line(&mut input).unwrap();
-
-    // let mut count = 0;
-
-    // while count < 2 {
-    //     let v: i32 = input.trim().parse::<i32>().unwrap();
-
-    //     println!("{}", count.to_string());
-
-    //     if v
-    //     count += 1;
-    // }
-
     let mut lines = io::stdin().lock().lines();
 
-    let mut i: Vec<i32> = Vec::new();
+    let mut i: Vec<String> = Vec::new();
     let mut count: i32 = 1;
 
     while let Some(line) = lines.next() {
-        let last_input: i32 = line.unwrap().parse::<i32>().unwrap();
+        let last_input: String = line.unwrap();
 
         i.push(last_input);
 
-        // stop reading
         if count >= 2 {
             break;
         }
@@ -34,5 +18,14 @@ fn main() {
         count += 1;
     }
 
-    println!("{} {} {}", i[0], i[1], i[0] * i[1]);
+    let left = i[0].parse::<i32>().unwrap();
+    let right = i[1].parse::<i32>().unwrap();
+
+    i[1].chars().rev().for_each(|c| {
+        let right = c.to_string().parse::<i32>().unwrap();
+
+        println!("{}", left * right);
+    });
+
+    println!("{}", left * right);
 }
