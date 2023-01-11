de7c1244d28dc238fb48f3f049c80ca69274cfaa
Seonzoo <boy672820@gmail.com>
2023-01-11 14:05:13 +0900

    fixed untracked files

--- loop_15552/seonzoo/target/debug/deps/loop_15552-fa8c3f455e9af089.d
+++
@@ -1,5 +0,0 @@
-/Users/seondev/Projects/ps-day/loop_15552/seonzoo/target/debug/deps/loop_15552-fa8c3f455e9af089: src/main.rs
-
-/Users/seondev/Projects/ps-day/loop_15552/seonzoo/target/debug/deps/loop_15552-fa8c3f455e9af089.d: src/main.rs
-
-src/main.rs:
--- loop_15552/seonzoo/target/debug/loop_15552.d
+++
@@ -1,1 +0,0 @@
-/Users/seondev/Projects/ps-day/loop_15552/seonzoo/target/debug/loop_15552: /Users/seondev/Projects/ps-day/loop_15552/seonzoo/src/main.rs
