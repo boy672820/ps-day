1b35bc3d0e0599f7fc71edc421b0d84183e3225f
Seonzoo <boy672820@gmail.com>
2023-01-24 21:33:40 +0900

    fixed untracked files

--- fn_1065/seonzoo/target/debug/deps/fn_1065-34f0535159056768.d
+++
@@ -1,5 +0,0 @@
-/Users/seondev/Projects/ps-day/fn_1065/seonzoo/target/debug/deps/fn_1065-34f0535159056768: src/main.rs
-
-/Users/seondev/Projects/ps-day/fn_1065/seonzoo/target/debug/deps/fn_1065-34f0535159056768.d: src/main.rs
-
-src/main.rs:
--- fn_1065/seonzoo/target/debug/fn_1065.d
+++
@@ -1,1 +0,0 @@
-/Users/seondev/Projects/ps-day/fn_1065/seonzoo/target/debug/fn_1065: /Users/seondev/Projects/ps-day/fn_1065/seonzoo/src/main.rs
