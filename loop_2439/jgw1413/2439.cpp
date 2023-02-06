1ebbef1668dda056f15c7f9fbc7eb8ae4d225935
jgw1413 <jgw1413@naver.com>
2023-02-06 19:49:23 +0900

    [조관우] 2438, 2439 풀이

---
+++ loop_2439/jgw1413/2439.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = i; j < N - 1; j++)
+		{
+			cout << " ";
+		}
+	
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "*";
+		}
+
+		cout << "\n";
+	}
+}
