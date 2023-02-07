d27209a15d59fbec06dd2b6f146e448df6a4a71d
jgw1413 <jgw1413@naver.com>
2023-02-07 12:10:41 +0900

    [조관우] 1330, 14681, 2480, 2525, 2753, 2884, 9498, 10950, 2739, 8393 풀이

---
+++ loop_10950/jgw1413/10950.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int main()
+{
+	int T, A, B;
+
+	cin >> T;
+
+	for (int i = 0; i < T; i++)
+	{
+		cin >> A >> B;
+
+		cout << A + B << "\n";
+	}
+}
