82ea681264c941d01fa34c0c72359b5ce62a5bb9
Seonzoo <boy672820@gmail.com>
2023-04-08 12:13:14 +0900

    new clear

---
+++ 11650/seonzoo/src/main.rs
@@ -0,0 +1,30 @@
+use io::Write;
+use std::io;
+
+fn main() {
+    let mut buf = String::new();
+    let mut out = io::BufWriter::new(io::stdout().lock());
+
+    io::stdin().read_line(&mut buf).unwrap();
+
+    let t = buf.trim().parse::<u32>().unwrap();
+    let mut v = Vec::new();
+
+    for _ in 0..t {
+        buf.clear();
+        io::stdin().read_line(&mut buf).unwrap();
+
+        let r = buf
+            .split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
+
+        v.push(r);
+    }
+
+    v.sort();
+
+    for i in v.iter() {
+        writeln!(out, "{} {}", i[0], i[1]);
+    }
+}
