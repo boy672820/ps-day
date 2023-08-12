1ee5c2a7605fb2b38e5d11ad0c21084a87d3cd83
Seonzoo <boy672820@gmail.com>
2023-08-13 01:08:39 +0900

    2590 진행중

--- 2590/baekjoon_2590/src/main.rs
+++ 2590/baekjoon_2590/src/main.rs
@@ -2,26 +2,132 @@ use std::io;
 
 fn main() {
     let mut buf = String::new();
-    let mut a: [usize; 6] = [0, 0, 0, 0, 0, 0];
+    let mut a: [i32; 6] = [0, 0, 0, 0, 0, 0];
 
     for i in 0..6 {
         buf.clear();
         io::stdin().read_line(&mut buf).unwrap();
-        a[i] = buf.trim().parse::<usize>().unwrap();
+        a[i] = buf.trim().parse::<i32>().unwrap();
     }
 
     let mut n = a[5];
 
-    while a[0] != 0 || a[1] != 0 || a[2] != 0 || a[3] != 0 || a[4] != 0 {
-        while a[4] > 0 {
-            let mut b = 36;
-            a[4] -= 1;
-            b -= 25;
+    let five = a[4];
+    a[0] -= five * 11;
 
-            a[0] = if a[0] <= b { 0 } else { a[0] - b };
-            n += 1;
+    if a[0] < 0 {
+        a[0] = 0;
+    }
+    n += five;
+
+    let four = a[3];
+    let mut cap_2by2 = four * 5;
+
+    if cap_2by2 < a[1] {
+        a[1] -= cap_2by2;
+    } else {
+        cap_2by2 -= a[1];
+        a[1] = 0;
+
+        let cap_1by1 = cap_2by2 * 4;
+        a[0] -= cap_1by1;
+
+        if a[0] < 0 {
+            a[0] = 0;
         }
     }
+    n += four;
+
+    let mut three = a[2] / 4;
+    if a[2] % 4 != 0 {
+        three += 1;
+    }
+
+    if a[2] % 4 == 1 {
+        let mut cap_2by2 = 5;
+
+        if a[1] > cap_2by2 {
+            a[1] -= cap_2by2;
+            let cap_1by1 = 9;
+            a[0] -= cap_1by1;
+
+            if a[0] < 0 {
+                a[0] = 0;
+            }
+        } else {
+            cap_2by2 -= a[1];
+            a[1] = 0;
+            let cap_1by1 = 9 + cap_2by2 * 4;
+            a[0] -= cap_1by1;
+
+            if a[0] < 0 {
+                a[0] = 0;
+            }
+        }
+    } else if a[2] % 4 == 2 {
+        let mut cap_2by2 = 3;
+
+        if a[1] > cap_2by2 {
+            a[1] -= cap_2by2;
+            let cap_1by1 = 6;
+            a[0] -= cap_1by1;
+
+            if a[0] < 0 {
+                a[0] = 0;
+            }
+        } else {
+            cap_2by2 -= a[1];
+            a[1] = 0;
+            let cap_1by1 = 6 + cap_2by2 * 4;
+            a[0] -= cap_1by1;
+
+            if a[0] < 0 {
+                a[0] = 0;
+            }
+        }
+    } else if a[2] % 4 == 3 {
+        let mut cap_2by2 = 1;
+
+        if a[1] > cap_2by2 {
+            a[1] -= cap_2by2;
+            let cap_1by1 = 5;
+            a[0] -= cap_1by1;
+
+            if a[0] < 0 {
+                a[0] = 0;
+            }
+        } else {
+            cap_2by2 -= a[1];
+            a[1] = 0;
+            let cap_1by1 = 5 + cap_2by2 * 4;
+            a[0] -= cap_1by1;
+
+            if a[0] < 0 {
+                a[0] = 0;
+            }
+        }
+    }
+    n += three;
+
+    let mut two = a[1] / 9;
+
+    if a[1] % 9 != 0 {
+        two += 1;
+        let cap_1by1 = 36 - 4 * (a[1] % 9);
+        a[0] -= cap_1by1;
+
+        if a[0] < 0 {
+            a[0] = 0;
+        }
+    }
+    n += two;
+
+    let mut one = a[0] / 36;
+
+    if a[0] % 36 != 0 {
+        one += 1;
+    }
+    n += one;
 
     println!("{}", n)
 }
