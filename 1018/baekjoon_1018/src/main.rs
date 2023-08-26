eb5499f19422bdde8f38875dc8e6d875cde868c9
Seonzoo <boy672820@gmail.com>
2023-08-26 19:59:28 +0900

    new clear

--- 1018/baekjoon_1018/src/main.rs
+++ 1018/baekjoon_1018/src/main.rs
@@ -1,7 +1,27 @@
+use std::cmp;
 use std::io;
 
-// const MAX_SIZE: usize = 51;
-const MAX_SIZE: usize = 5;
+const MAX_SIZE: usize = 51;
+const WHITE_BOARD: [[usize; 8]; 8] = [
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+];
+const BLACK_BOARD: [[usize; 8]; 8] = [
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+];
 
 fn main() {
     let mut buf = String::new();
@@ -16,13 +36,13 @@ fn main() {
     };
 
     // 0 = White, 1 = Black
-    let mut a: [[usize; MAX_SIZE]; MAX_SIZE] = [[0; MAX_SIZE]; MAX_SIZE];
+    let mut board: [[usize; MAX_SIZE]; MAX_SIZE] = [[0; MAX_SIZE]; MAX_SIZE];
 
-    for m in 0..m {
+    for i in 0..n {
         buf.clear();
         io::stdin().read_line(&mut buf).unwrap();
-        for n in 0..n {
-            a[m][n] = if buf.as_bytes()[n] as char == 'W' {
+        for j in 0..m {
+            board[i][j] = if buf.as_bytes()[j] as char == 'W' {
                 0
             } else {
                 1
@@ -30,5 +50,32 @@ fn main() {
         }
     }
 
-    println!("{:?}", a);
+    let mut r = 37;
+    for y in 0..n - 7 {
+        for x in 0..m - 7 {
+            let [w, b] = count_chess_board(board, x, y);
+            let cmp = cmp::min(w, b);
+            if cmp < r {
+                r = cmp;
+            }
+        }
+    }
+
+    println!("{}", r)
+}
+
+fn count_chess_board(board: [[usize; MAX_SIZE]; MAX_SIZE], x: usize, y: usize) -> [usize; 2] {
+    let mut white_count = 0;
+    let mut black_count = 0;
+    for i in 0..8 {
+        for j in 0..8 {
+            if WHITE_BOARD[i][j] != board[y + i][x + j] {
+                white_count += 1;
+            }
+            if BLACK_BOARD[i][j] != board[y + i][x + j] {
+                black_count += 1;
+            }
+        }
+    }
+    [white_count, black_count]
 }
