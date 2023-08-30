f844c1f05788edde0bd60bfa5d346c6dd5f8b3af
Seonzoo <boy672820@gmail.com>
2023-08-31 01:24:28 +0900

    new clear:

--- 10816/baekjoon_10816/src/main.rs
+++ 10816/baekjoon_10816/src/main.rs
@@ -25,18 +25,43 @@ fn main() {
         .map(|x| x.trim().parse::<i32>().unwrap())
         .collect::<Vec<_>>()[..m];
 
+    let mut string = String::new();
+
     for i in 0..m {
-        print!("{} ", cmp_v[i]);
+        let k = cmp_v[i];
+        let r = upper_boundary(k, &v) - lower_boundary(k, &v);
+        string.push_str(&r.to_string());
+        string.push_str(" ");
     }
+
+    println!("{}", &string[..string.len() - 1])
 }
 
-fn lower_boundary(k: i32, v: &Vec<&i32>) -> u32 {
-    let (mut lo, mut hi) = (0, v.len() as i32);
+fn lower_boundary(k: i32, v: &Vec<&i32>) -> i32 {
+    let mut lo = 0;
+    let mut hi = v.len() as i32;
 
     while lo < hi {
         let mid = (lo + hi) / 2;
 
         if k <= *v[mid as usize] {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+
+    lo
+}
+
+fn upper_boundary(k: i32, v: &Vec<&i32>) -> i32 {
+    let mut lo = 0;
+    let mut hi = v.len() as i32;
+
+    while lo < hi {
+        let mid = (lo + hi) / 2;
+
+        if k < *v[mid as usize] {
             hi = mid;
         } else {
             lo = mid + 1;
