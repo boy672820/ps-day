b77d217518eea90d94c4aefaa70919d38dfe303a
Seonzoo <boy672820@gmail.com>
2023-11-25 15:50:33 +0900

    New clear

---
+++ 11727/baejoon_11727/src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+const MODULAR: usize = 10_007;
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+
+    let n = buf.trim().parse::<usize>().unwrap();
+
+    let mut memo: [usize; 1001] = [0; 1001];
+    memo[1] = 1;
+    memo[2] = 3;
+
+    for i in 3..1001 {
+        memo[i] = (memo[i - 2] * 2 + memo[i - 1]) % MODULAR;
+    }
+
+    println!("{}", memo[n]);
+}
